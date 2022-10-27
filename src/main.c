caf780bebb097a6482bf5df052aca705f986f66c
lucasexequiel <lucasexequiel111@gmail.com>
2022-10-27 00:41:40 -0300

    First comunication 100KHz

--- src/main.c
+++ src/main.c
@@ -2,82 +2,76 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 
-static const char *TAG = "i2c-example";
 
-#define I2C_SLAVE_ADDR 0x60//0xC1
+#define I2C_SLAVE_ADDR 0x62//0xC1
 #define TIMEOUT_MS 1000
 #define DELAY_MS 100
-#define ACK_CHECK_EN 0x1
-#define I2C_MASTER_PORT I2C_NUM_1
+#define ACK_CHECK_EN 0
 
-static esp_err_t i2c_master_init()
+
+static void i2c_master_init()
 {
 
     // i2c_port_t i2c_master_port = I2C_NUM_0;
 
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = GPIO_NUM_21,
+        .sda_io_num = 21,
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_io_num = GPIO_NUM_22,
+        .scl_io_num = 22,
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
         .master.clk_speed = 400000, // Fast Mode 400Kbit/s mcp4725
         // .master.clk_speed = 3400000, // High Speed Mode 3.4Mbit/s mcp4725
     };
-    esp_err_t err = i2c_param_config(I2C_MASTER_PORT, &conf);
-    if (err != ESP_OK)
-    {
-        return err;
-    }
-    return i2c_driver_install(I2C_MASTER_PORT, conf.mode, 0, 0, 0);
+
+    i2c_param_config(I2C_NUM_0, &conf);
+    
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+    return;
 }
+
+
+
+
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C Initialized succesfully");
+    gpio_set_direction(GPIO_NUM_0,GPIO_MODE_INPUT);
+    gpio_set_pull_mode(0, GPIO_FLOATING);
 
-    // uint8_t rx_data [2];
-    // rx_data[1]  = 0b00000000;
-    // rx_data[0]  = 0b00000000;
+    printf ("inicio\n");
+    int boton = gpio_get_level(0);
+
+    printf("Antes de while, boton igual a: %d \n", boton);
+
+
+    i2c_master_init();
 
     esp_err_t i2c_ret = ESP_OK;
 
-    while (1)
-    {
-        // Command link created
-        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-        // Start condition
-        i2c_master_start(cmd);
-        // Address + Write bit 
-        /*----------------------SE HACE ESTE CORRIMIENTO HACIA LA IZQUIERDA PORQUE EL TAMAÑO DEL CANAL DE COMUNICACION DEL I2C---------------------------*/
-        ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (I2C_SLAVE_ADDR << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN)); // habla al sensor dac
-        i2c_master_write_byte(cmd, 0x02, ACK_CHECK_EN);
-        i2c_master_write_byte(cmd, 0x7f, ACK_CHECK_EN);
+    uint8_t rx[2];
+    rx[1]  = 0b11111111;
+    rx[0]  = 0b00001111;
 
-        // ESP_ERROR_CHECK(i2c_master_write_byte(cmd,I2C_SLAVE_ADDR, ACK_CHECK_EN));
-        // i2c_master_write(cmd, rx_data, 2, ACK_CHECK_EN);                                    // manda el dato
-        
-        i2c_master_stop(cmd);
-        i2c_ret = i2c_master_cmd_begin(I2C_MASTER_PORT, cmd, 1000 / portTICK_PERIOD_MS);
-        i2c_cmd_link_delete(cmd);
+    
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (I2C_SLAVE_ADDR << 1) | I2C_MASTER_WRITE, 0); 
+    i2c_master_write_byte(cmd, rx[0], ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, rx[1], ACK_CHECK_EN);
+    i2c_master_stop(cmd);
 
-        // i2c_master_read_from_device(I2C_NUM_0, I2C_SLAVE_ADDR, rx_data, 5, TIMEOUT_MS / portTICK_RATE_MS);
-        // Similar a la API de envío de datos, debe especificar el número de puerto I2C, la dirección del esclavo, el búfer de recepción y el valor de tiempo de espera.
-        // description tag, buffer Pointer to the buffer array y tamaño de buffer en bytes.
-        // ESP_LOG_BUFFER_HEX(TAG, rx_data, 2);
+    // while (boton == 1)
+    // {
+    //     if (aux == 0){
+    //     printf("%d \n", boton);
+    //     }
+    //     aux = 1;
+    // };
 
-        
-
-        if (i2c_ret == ESP_OK)
-        {
-            // READ BYTES
-            printf("I2C Write OK\n");
-        }
-        else
-        {
-            printf("I2C FAILED\n");
-        }
-
-        }
+    i2c_ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    printf("%d \n", i2c_ret);
+    printf ("fin\n");
+    
 }
