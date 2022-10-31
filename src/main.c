87277fa87a52d82ed00d36b19e9cb44bac103d18
lucasexequiel <lucasexequiel111@gmail.com>
2022-10-31 09:56:49 -0300

    Initial voltage in DAC is 0v

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 #define DELAY_MS 100
 #define ACK_CHECK_EN 0
 
-
 static void i2c_master_init()
 {
 
@@ -27,35 +26,39 @@ static void i2c_master_init()
     return;
 }
 
+
+static void i2c_valor(){
+
+    uint8_t rx[2];
+    rx[1] = 0b11111111;
+    rx[0] = 0b00000111;
+
+    while(1){
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (I2C_SLAVE_ADDR << 1) | I2C_MASTER_WRITE, 0);
+    i2c_master_write_byte(cmd, rx[0], ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, rx[1], ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_NUM_0, cmd, 100 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    printf("Se envio de valor I2C con valor 3.3V\n\n");
+    vTaskDelay(100);
+    return;
+    }
+}
+
 void app_main(void)
 {
-    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(0, GPIO_FLOATING);
-    printf("Inicio\n");
-
     // Seteo de GPIO
     i2c_master_init();
 
-    // Valor deseado en la salida del dac
-    uint8_t rx[3];
-    rx[2] = 0b00001111;
-    rx[1] = 0b00000000;
-    rx[0] = 0b01100000;
-
-
-    // Test velocidad de respuesta
-    while (1)
+    for (size_t i = 0; i < 4; i++)
     {
-        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-        i2c_master_start(cmd);
-        i2c_master_write_byte(cmd, (I2C_SLAVE_ADDR << 1) | I2C_MASTER_WRITE, 0);
-        i2c_master_write_byte(cmd, rx[0], ACK_CHECK_EN);
-        i2c_master_write_byte(cmd, rx[1], ACK_CHECK_EN);
-        i2c_master_write_byte(cmd, rx[2], ACK_CHECK_EN);
-        i2c_master_stop(cmd);
-        i2c_master_cmd_begin(I2C_NUM_0, cmd, 100 / portTICK_PERIOD_MS);
-        i2c_cmd_link_delete(cmd);
-        printf("Byte enviado x 4 \n");
-        vTaskDelay(100);
+        vTaskDelay(500);
+        printf("a la [%d]\n\n", i);
     }
+
+    i2c_valor();
+
 }
